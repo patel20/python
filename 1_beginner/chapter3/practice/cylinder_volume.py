12c9f80a5a2c180cd7e0669060c0bce91a7ecf8d
patel20 <67443596+patel20@users.noreply.github.com>
2020-06-25 15:44:44 -0700

    Aayushya Patel
    This is the answer to my homework.

--- 1_beginner/chapter3/practice/cylinder_volume.py
+++ 1_beginner/chapter3/practice/cylinder_volume.py
@@ -3,3 +3,12 @@
 # height and radius of a cylinder and returns the volume
 
 # write code here
+#aayushya Patel
+import math
+Hight = int(input("What is the hight of your cylinder?:"))
+radius = int(input("What is the radius of your cylinder?:"))
+radius ** 2
+radius *= 3.141
+ans = str(radius*Hight)
+s = str("^3")
+print (ans + s)
