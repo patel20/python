2acb1c7a597864a4ab85faa56b5ee26115f2fc9a
patel20 <67443596+patel20@users.noreply.github.com>
2020-06-25 15:52:58 -0700

    Aayushya Patel
    This is the answer to my homework.

--- 1_beginner/chapter3/practice/circle.py
+++ 1_beginner/chapter3/practice/circle.py
@@ -4,3 +4,10 @@
 # Store 3.14 as the value of pi, which is a constant.
 
 # write your code here
+import math
+radius = int(input("What is the radius of your cylinder?:"))
+radius ** 2
+radius *= 3.141
+ans = str(radius)
+s = str("^2")
+print (ans + s)
